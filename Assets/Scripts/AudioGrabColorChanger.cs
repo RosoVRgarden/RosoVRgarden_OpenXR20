56b705d76a301992a6a7ddfd04c27821cd44bfaa
RosoVRgarden <rosovrgarden@gmail.com>
2022-05-06 19:53:48 +0800

    script cleaning

--- Assets/Scripts/AudioGrabColorChanger.cs
+++ Assets/Scripts/AudioGrabColorChanger.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 // XRBaseInteractable - OBJECT
@@ -19,11 +20,11 @@ using UnityEngine.XR.Interaction.Toolkit;
         originalMaterial = meshRenderer.material;
 		
         interactable = GetComponent<XRBaseInteractable>();
-        interactable.onHoverEntered.AddListener(StartAudio);
-        interactable.onHoverExited.AddListener(StopAudio);	
+        interactable.hoverEntered.AddListener(StartAudio);
+        interactable.hoverExited.AddListener(StopAudio);	
 	}
 
-	void Start(){
+    void Start(){
 		audioData = GetComponent<AudioSource>();
 		audioData.Play(0);
 		audioData.Pause();
@@ -34,22 +35,21 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 	private void OnDestroy()
 	{	
-		interactable.onHoverEntered.RemoveListener(StartAudio); 
-		interactable.onHoverExited.RemoveListener(StopAudio);
+		interactable.hoverEntered.RemoveListener(StartAudio); 
+		interactable.hoverExited.RemoveListener(StopAudio);
 	}
-	
-	
-	private void StartAudio(XRBaseInteractor interactor)
+
+	private void StartAudio(HoverEnterEventArgs interactor)
 	{
 		meshRenderer.material = selectMaterial;
 		audioData.UnPause();
 	}
 
-	private void StopAudio(XRBaseInteractor interactor)
+	private void StopAudio(HoverExitEventArgs interactor)
 	{
 		meshRenderer.material = originalMaterial;
 		audioData.Pause();
 		Debug.Log("Pause: " + audioData.time);
-		
 	}
+
 }
